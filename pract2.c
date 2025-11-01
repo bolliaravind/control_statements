760b462bbb26527fac6802bc614f88a861db128e
bolliaravind <bolliaravind5@gmail.com>
2025-11-01 16:46:18 +0530

    question num1

---
+++ pract2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a;
+float b;
+printf("enter the integer value: ");
+scanf("%d",&a);
+printf("enter the float value: ");
+scanf("%f",&b);
+printf("integer value is %d\n",a);
+printf("float value is %f",b); 
+return 0;
+}
