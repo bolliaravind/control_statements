cc83af36ddc5133b000d72f405ad59295fd80ec7
bolliaravind <bolliaravind5@gmail.com>
2025-11-02 16:45:40 +0530

    whether given numbers are equal or not

---
+++ equal numbers.c
@@ -0,0 +1,14 @@
+//To accept two integer and check whether they are equal or not
+#include<stdio.h>
+int main(){
+int num1,num2;
+printf("enter the num 1: ");
+scanf("%d",&num1);
+printf("enter the num 2: ");
+scanf("%d",&num2);
+if(num1==num2){
+  printf("both numbers are equal\n");
+}else{
+  printf("number are not equal\n");
+  }
+  }
