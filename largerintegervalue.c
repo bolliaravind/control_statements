fe2a187f2be6e50895a9c77692737a773254b16c
bolliaravind <bolliaravind5@gmail.com>
2025-11-07 11:18:19 +0530

    WRITE A C PROGRAM TO READ THE VALUE OF AN INTEGER M AND DISPLAY THE VALUE OF N IS 1 WHEN M IS LARGER THAN 0, 0 WHEN M IS 0 AND -1 WHEN M IS LESS THAN 0.

---
+++ largerintegervalue.c
@@ -0,0 +1,16 @@
+//WRITE A C PROGRAM TO READ THE VALUE OF AN INTEGER M AND DISPLAY THE VALUE OF N
+//IS 1 WHEN M IS LARGER THAN 0, 0 WHEN M IS 0 AND -1 WHEN M IS LESS THAN 0.
+#include<stdio.h>
+int main(){
+int m,n;
+printf("enter the integer value for m: ");
+scanf("%d",&m);
+if(m>0)
+  n=1;
+else if (m==0)
+  n=0;
+else
+  n=-1;
+printf("the value of n is %d\n",n);
+return 0;
+}
