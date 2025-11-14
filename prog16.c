666d613c854f589113772d9fd7b7bb9937c50cc9
bolliaravind <bolliaravind5@gmail.com>
2025-11-14 17:18:45 +0530

    WRITE A C PROGRAM TO CALCULATE THE SUM OF NUMBERS FROM 1 TO 100 USING A WHILE LOOP?

---
+++ prog16.c
@@ -0,0 +1,10 @@
+//WRITE A C PROGRAM TO CALCULATE THE SUM OF NUMBERS FROM 1 TO 100 USING A WHILE LOOP?
+#include<stdio.h>
+int main(){
+int i=1,sum=0;
+while(i<=100){
+  sum+=i;
+  i++;
+  }
+printf("the sum of numbers from1 to 100 is=%d",sum);
+}
